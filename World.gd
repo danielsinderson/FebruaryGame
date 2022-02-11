eca943dfb3a27662d235f33990f71f1d3886f706
scraptured <drsinderson@gmail.com>
2022-02-11 08:45:35 -0800

    Added pits; Added player equipment list

--- World.gd
+++ World.gd
@@ -12,6 +12,8 @@ onready var pathfinding := $Pathfinding
 func _ready():
 	pathfinding.create_navigation_map(tilemap, false)
 	pathfinding.add_to_excluded_groups("obstacles")
+	if "torch" in player.equipment:
+		pathfinding.add_to_excluded_groups("pits")
 	var new_path = pathfinding.get_new_path(player.position, goal.position)
 	player.path = new_path
 	
