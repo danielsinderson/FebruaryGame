9d16c3620cc210d9962ee5099aaca07410af2f84
scraptured <drsinderson@gmail.com>
2022-02-12 04:32:47 -0800

    Added Player Death on Pits
     - World.gd now calculates a list of ids for all cells where a pit tile is placed on _ready()
     - On _physics_process() World.gd now finds the Player node's position, translates it into a pathfinding cell id, then checks to see if it matches any of the pit tile ids: if yes, it clears the Player node and changes the scene back to the equipment menu

--- World.gd
+++ World.gd
@@ -3,17 +3,33 @@ extends Node2D
 
 onready var player := $Player
 onready var goal := $Goal
-onready var tilemap := $Floor
+onready var floor_tiles := $Floor
+onready var pit_tiles := $Pits
 onready var pathfinding := $Pathfinding
+
+var player_position := Vector2.ZERO
+var pit_tiles_cells := []
+var pit_tile_cell_ids := []
 
 
 
 
 func _ready():
-	pathfinding.create_navigation_map(tilemap, false)
+	pathfinding.create_navigation_map(floor_tiles, false)
 	pathfinding.add_to_excluded_groups("obstacles")
 	if "torch" in player.equipment:
 		pathfinding.add_to_excluded_groups("pits")
 	var new_path = pathfinding.get_new_path(player.position, goal.position)
 	player.path = new_path
 	
+	pit_tiles_cells = pit_tiles.get_used_cells()
+	for cell in pit_tiles_cells:
+		pit_tile_cell_ids.append(pathfinding.get_id_for_point(cell))
+
+
+func _physics_process(delta):
+	player_position = player.global_position
+	var player_cell = floor_tiles.world_to_map(player_position)
+	if pathfinding.get_id_for_point(player_cell) in pit_tile_cell_ids:
+		player.queue_free()
+		get_tree().change_scene_to(load("res://GameObjects/Menu_Equipment.tscn"))
