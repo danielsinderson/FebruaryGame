33d05bb284477ed8da09b22636d33690ed153c3f
scraptured <drsinderson@gmail.com>
2022-02-12 06:31:33 -0800

    Implemented Player Animations
    Just what it says on the tin. =)

--- GameObjects/Player.gd
+++ GameObjects/Player.gd
@@ -5,10 +5,8 @@ var path := PoolVector2Array() setget set_path
 var index = 1
 var equipment := GameData.equipment_chosen
 
+var facing := Vector2(0, 1)
 
-
-func _process(delta):
-	pass
 
 
 func move_along_path():
@@ -17,15 +15,32 @@ func move_along_path():
 		global_position = path[index]
 		if index < length-1:
 			index += 1
-		else:
-			set_process(false)
+			set_sprite()
+			
+			
+			
+			
 
 
 func set_path(value : PoolVector2Array):
 	path = value
 	if value.size() == 0:
 		return
+
+
+func set_sprite():
+	var facing = path[index] - path[index-1]
+	if facing.y > 0:
+		$PlayerSprite.frame_coords.y = 1
+	elif facing.y < 0:
+		$PlayerSprite.frame_coords.y = 2
+	else:
+		$PlayerSprite.frame_coords.y = 3
 	
+	if "sword" in equipment:
+		$PlayerSprite.frame_coords.x = (index % 8) + 16
+	elif "torch" in equipment:
+		$PlayerSprite.frame_coords.x = (index % 8) + 8
 
 
 func _on_Timer_timeout():
